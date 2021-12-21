8af4d5c7994c710861c97099e9be1332629b944d
NguyenVanTrung612 <vantrung060101@gmail.com>
2021-12-21 10:42:31 +0700

    init

--- TutorialJS/weather-app/js/index.js
+++ TutorialJS/weather-app/js/index.js
@@ -1,13 +1,32 @@
 const APP_ID = `3f37c355123b5ebde1625f24562d66b3`;
+const DEFAULT_VALUE = "--";
 
 const searchInput = document.querySelector("#search-input");
+const cityName = document.querySelector(".city-name");
+const weatherStatus = document.querySelector(".weather-status");
+const weatherIcon = document.querySelector(".weather-icon");
+const temperature = document.querySelector(".temperature");
+
+const sunrise = document.querySelector(".sunrise");
+const sunset = document.querySelector(".sunset");
+const humidity = document.querySelector(".humidity");
+const windSpeed = document.querySelector(".windspeed");
+
 searchInput.addEventListener("change", (event) => {
   // Test input when user write and submit enter
   //   console.log(["searchInput"], event);
   fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${event.target.value}&appid=${APP_ID}&units=metric`
+    `https://api.openweathermap.org/data/2.5/weather?q=${event.target.value}&appid=${APP_ID}&units=metric&lang=vi`
   ).then(async (result) => {
     const data = await result.json();
-    console.log(["search input"], data);
+    // console.log(["search input"], data);
+    // if data not return name, get default value
+    cityName.innerHTML = data.name || DEFAULT_VALUE;
+    weatherStatus.innerHTML = data.weather[0].description || DEFAULT_VALUE;
+    weatherIcon.setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+    );
+    temperature.innerHTML = Math.round(data.main.temp) || DEFAULT_VALUE;
   });
 });
